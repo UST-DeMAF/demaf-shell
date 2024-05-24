adee4a31953d3518175cb01fe53a4588a17a70db
Marcel Weller <marcefrederik@aol.de>
2024-05-24 15:33:56 +0200

    adjusted test to not block build process

--- src/test/java/ust/demaf/demafshell/DemafShellApplicationTests.java
+++ src/test/java/ust/demaf/demafshell/DemafShellApplicationTests.java
@@ -1,9 +1,9 @@
 package ust.demaf.demafshell;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+
+
 class DemafShellApplicationTests {
 
 	@Test
