ffc29176cdbe86bed6e66ac4549d66fc2ab04336
Nils <boike.nils@gmail.com>
2024-08-07 18:26:05 +0200

    Added option to kill queue by position. E.g. 'purge 1'

--- src/main/java/ust/demaf/demafshell/commands/RabbitMQCommand.java
+++ src/main/java/ust/demaf/demafshell/commands/RabbitMQCommand.java
@@ -1,7 +1,6 @@
 package ust.demaf.demafshell.commands;
 
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.shell.standard.ShellCommandGroup;
 import org.springframework.shell.standard.ShellComponent;
@@ -15,26 +14,36 @@ import java.util.List;
 @ShellCommandGroup("Purge Commands")
 public class RabbitMQCommand {
 
-    @Autowired
-    private RabbitTemplate rabbitTemplate;
 
     @Autowired
     private RabbitAdmin rabbitAdmin;
 
     @ShellMethod("Purge the message queue.")
-    public String purgeQueue(
-            @ShellOption(value = {"-q", "--queue"},
-                help = "The name of the queue to purge.")
-                String queueName) {
+    public String purge(String queueIdentifier) {
+        List<String> queueNames = rabbitMQService.getQueueNames();
+        String queueName;
+
+        try {
+            int position = Integer.parseInt(queueIdentifier);
+            if (position < 1 || position > queueNames.size()) {
+                return "Invalid queue position.";
+            }
+            queueName = queueNames.get(position - 1);
+        } catch (NumberFormatException e) {
+            queueName = queueIdentifier;
+            if (!queueNames.contains(queueName)) {
+                return "Queue name not found.";
+            }
+        }
         rabbitAdmin.purgeQueue(queueName, false);
-        return "Queue purged.";
+        return "Queue " + queueName + " purged.";
     }
 
     @Autowired
     private RabbitMQService rabbitMQService;
 
     @ShellMethod("List all available RabbitMQ queues.")
-    public List<String> listQueues() {
+    public List<String> listq() {
         return rabbitMQService.getQueueNames();
     }
 }
